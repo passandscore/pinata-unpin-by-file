9ea0a51f8ee1c88cc6f7f3e45bca10ee96788a07
Jason Schwarz <jason.c.schwarz@gmail.com>
2023-06-20 12:53:35 -0300

    Add timer to handle rate limiting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{ fs::File, io::{ self, BufRead, BufReader } };
+use std::{ fs::File, io::{ self, BufRead, BufReader, Write }, thread, time::{ Duration, Instant } };
 
 mod unpin_by_hash;
 use colored::Colorize;
@@ -33,11 +33,38 @@ fn main() -> io::Result<()> {
             }
         }
 
+        let mut counter = 0;
+        let interval_duration = Duration::from_secs(60);
+        let mut interval_start = Instant::now();
+
         for (index, hash) in hashes.iter().skip(1).enumerate() {
             let hash_without_quotes = hash.trim_matches('"');
 
-            if let Err(_error) = unpin_by_hash::main_with_args(hash_without_quotes, index + 1) {
+            if counter >= 10 {
+                let elapsed = interval_start.elapsed();
+                if elapsed < interval_duration {
+                    let remaining = interval_duration - elapsed;
+                    println!();
+                    print!("{} ", "Waiting for 60 seconds".bold().red());
+                    for _ in 0..remaining.as_secs() {
+                        print!(".");
+                        io::stdout().flush().unwrap();
+                        thread::sleep(Duration::from_secs(2));
+                    }
+                    println!();
+                }
+                interval_start = Instant::now();
+                counter = 0;
             }
+
+            if let Err(_error) = unpin_by_hash::main_with_args(hash_without_quotes, index + 1) {
+                // Handle the error
+                println!();
+                println!("{} {}", "Error:".bold().red(), _error);
+                println!();
+            }
+
+            counter += 1;
         }
 
         println!();
